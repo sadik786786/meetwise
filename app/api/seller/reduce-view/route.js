fec8b98c90660a7aac1b470e8a199c848579bbe3
Sadik Ansari <ansarisadik2006@gmail.com>
2026-01-06 16:51:37 +0530

    Fix auth / update subscription logic / UI changes

--- app/api/seller/reduce-view/route.js
+++ app/api/seller/reduce-view/route.js
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import pool from "@/app/lib/db";
 import { getServerSession } from "next-auth";
-import { authOptions } from "@/app/lib/auth";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 
 export async function POST() {
   const session = await getServerSession(authOptions);
